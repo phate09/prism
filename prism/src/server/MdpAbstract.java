344339a6a984cf9e95accd48760f5ad55c0e70ce
edoardo <phate09@hotmail.it>
2020-01-29 11:09:57 +0000

    added check_state_list changed run behaviour

--- prism/src/server/MdpAbstract.java
+++ prism/src/server/MdpAbstract.java
@@ -6,9 +6,11 @@ import explicit.MDPSimple;
 import explicit.ModelCheckerResult;
 import explicit.rewards.MDPRewardsSimple;
 import prism.PrismException;
+import prism.PrismSettings;
 import py4j.GatewayServer;
 
 import java.util.BitSet;
+import java.util.List;
 
 public class MdpAbstract {
 	private MDPSimple mdpSimple;
@@ -24,6 +26,7 @@ public class MdpAbstract {
 	}
 
 	public MDPSimple getMdpSimple() {
+		System.out.println("New connnection");
 		return mdpSimple;
 	}
 
@@ -44,11 +47,43 @@ public class MdpAbstract {
 		return new Distribution();
 	}
 
-	public double[] check_property() {
+	public void export_to_dot_file() {
 		try {
+			mdpSimple.exportToDotFile("./mdp_simple_dot.dot");
+			System.out.println("File exported");
+		} catch (PrismException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public double[] check_property(int state_number) {
+		try {
+			if (mdpSimple.getNumInitialStates() == 0)
+				mdpSimple.addInitialState(0);
+			mdpSimple.findDeadlocks(true);
+			MDPModelChecker checker = new MDPModelChecker(null);
+			if (checker.getSettings() == null)
+				checker.setSettings(new PrismSettings());
+			BitSet bitSet = new BitSet();
+			bitSet.set(state_number);
+			final ModelCheckerResult modelCheckerResult = checker.computeReachProbs(mdpSimple, bitSet, true);
+			return modelCheckerResult.soln;
+		} catch (PrismException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public double[] check_state_list(int[] target_states_id) {
+		try {
+			if (mdpSimple.getNumInitialStates() == 0)
+				mdpSimple.addInitialState(0);
+			mdpSimple.findDeadlocks(true);
 			MDPModelChecker checker = new MDPModelChecker(null);
 			BitSet bitSet = new BitSet();
-			bitSet.set(3076);
+			for (int i : target_states_id) {
+				bitSet.set(i);
+			}
 			final ModelCheckerResult modelCheckerResult = checker.computeReachProbs(mdpSimple, bitSet, true);
 			return modelCheckerResult.soln;
 		} catch (PrismException e) {
